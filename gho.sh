8f2a9127079dcc0f238db928f122af90f9d2a8fd
rami <rami@cs.wisc.edu>
2020-09-26 17:56:29 -0500

    fml

---
+++ gho.sh
@@ -0,0 +1,3 @@
+export LEBENCH_DIR=~/LEBench
+export PATH=/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
+sudo python ${LEBENCH_DIR}/run.py | tee -a ${LEBENCH_DIR}/logs/run_$(date '+%m%d%Y_%H%M%S').txt
