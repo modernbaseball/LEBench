01da474f9bdef7bfa95f6aa669fda3c746181f95
rami <rami@cs.wisc.edu>
2020-09-26 18:45:57 -0500

    please work

--- gho.sh
+++ gho.sh
@@ -1,3 +1,6 @@
+# this fix didnt even work but i will do it anyway just in case
+if [ ! -e /TEST_DIR ]; then mkdir /TEST_DIR; fi
+
 export LEBENCH_DIR=~/LEBench/
 export PATH=/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
 sudo python ${LEBENCH_DIR}/run.py | tee -a ${LEBENCH_DIR}/logs/run_$(date '+%m%d%Y_%H%M%S').txt
