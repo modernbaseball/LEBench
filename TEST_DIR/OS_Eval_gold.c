18ebd351eace491a33c999c4f421bfe03c9e1c47
rami <rami@cs.wisc.edu>
2020-09-27 16:27:21 -0500

    im fed up

--- TEST_DIR/OS_Eval_gold.c
+++ TEST_DIR/OS_Eval_gold.c
@@ -1153,6 +1153,33 @@ int main(int argc, char *argv[])
 	
 	testInfo info;	
 
+	/*****************************************/
+	/*               GETPID                  */
+	/*****************************************/
+
+	sleep(60);
+	info.iter = BASE_ITER * 100;
+	info.name = "ref";
+	one_line_test(fp, copy, ref_test, &info);
+
+	info.iter = 100;
+	info.name = "cpu";
+	one_line_test(fp, copy, cpu_test, &info);
+
+
+	info.iter = BASE_ITER * 100;
+	info.name = "getpid";
+	one_line_test(fp, copy, getpid_test, &info);
+
+
+	
+	/*****************************************/
+	/*            CONTEXT SWITCH             */
+	/*****************************************/
+	info.iter = BASE_ITER * 10;
+	info.name = "context siwtch";
+	one_line_test(fp, copy, context_switch_test, &info);
+
 
 	/*****************************************/
 	/*             SEND & RECV               */
@@ -1182,6 +1209,186 @@ int main(int argc, char *argv[])
 	info.name = "big recv";
 	one_line_test_v2(fp, copy, recv_test, &info);
 	
+
+	/*****************************************/
+	/*         FORK & THREAD CREATE          */
+	/*****************************************/
+	info.iter = BASE_ITER * 2;
+	info.name = "fork";
+	two_line_test(fp, copy, forkTest, &info);
+	
+	info.iter = BASE_ITER * 5;
+	info.name = "thr create";
+	two_line_test(fp, copy, threadTest, &info);
+
+
+	int page_count = 6000;
+	void *pages[page_count];
+	for (int i = 0; i < page_count; i++) {
+    		pages[i] = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	}
+	
+	info.iter = BASE_ITER / 2;	
+	info.name = "big fork";
+	two_line_test(fp, copy, forkTest, &info);
+
+	for (int i = 0; i < page_count; i++) {
+		munmap(pages[i], PAGE_SIZE);
+	}
+
+	page_count = 12000;
+	printf("Page count: %d.\n", page_count);
+	void *pages1[page_count];
+	for (int i = 0; i < page_count; i++) {
+    		pages1[i] = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	}
+	
+	info.iter = BASE_ITER / 2;	
+	info.name = "huge fork";
+	two_line_test(fp, copy, forkTest, &info);
+
+	for (int i = 0; i < page_count; i++) {
+		munmap(pages1[i], PAGE_SIZE);
+	}
+
+
+	/*****************************************/
+	/*     WRITE & READ & MMAP & MUNMAP      */
+	/*****************************************/
+
+	/****** SMALL ******/
+	file_size = PAGE_SIZE;	
+	printf("file size: %d.\n", file_size);
+
+	info.iter = BASE_ITER * 10;
+	info.name = "small write";
+	one_line_test(fp, copy, write_test, &info);
+      
+	info.iter = BASE_ITER * 10; 
+	info.name = "small read";
+	read_warmup();
+	one_line_test(fp, copy, read_test, &info);
+	
+	info.iter = BASE_ITER * 10;
+	info.name = "small mmap";
+	one_line_test(fp, copy, mmap_test, &info);
+	
+	info.iter = BASE_ITER * 10;
+	info.name = "small munmap";
+	one_line_test(fp, copy, munmap_test, &info);
+
+	info.iter = BASE_ITER * 5;
+	info.name = "small page fault";
+	one_line_test(fp, copy, page_fault_test, &info);
+
+	/****** MID ******/
+	file_size = PAGE_SIZE * 10;
+	printf("file size: %d.\n", file_size);
+
+	info.iter = BASE_ITER * 10;
+	info.name = "mid write";
+	one_line_test(fp, copy, write_test, &info);
+	
+	info.iter = BASE_ITER * 10;
+	info.name = "mid read";
+	read_warmup();
+	one_line_test(fp, copy, read_test, &info);
+
+	info.iter = BASE_ITER * 10;
+	info.name = "mid mmap";
+	one_line_test(fp, copy, mmap_test, &info);
+	
+	info.iter = BASE_ITER * 10;
+	info.name = "mid munmap";
+	one_line_test(fp, copy, munmap_test, &info);
+
+	info.iter = BASE_ITER * 5;
+	info.name = "mid page fault";
+	one_line_test(fp, copy, page_fault_test, &info);
+
+	/****** BIG ******/
+	file_size = PAGE_SIZE * 1000;	
+	printf("file size: %d.\n", file_size);
+
+	info.iter = BASE_ITER / 2;
+	info.name = "big write";
+	one_line_test(fp, copy, write_test, &info);
+	
+	info.iter = BASE_ITER;
+	info.name = "big read";
+	read_warmup();
+	one_line_test(fp, copy, read_test, &info);
+	
+	info.iter = BASE_ITER * 10;
+	info.name = "big mmap";
+	one_line_test(fp, copy, mmap_test, &info);
+	
+	info.iter = BASE_ITER / 4;
+	info.name = "big munmap";
+	one_line_test(fp, copy, munmap_test, &info);
+	
+	info.iter = BASE_ITER * 5;
+	info.name = "big page fault";
+	one_line_test(fp, copy, page_fault_test, &info);
+
+       /****** HUGE ******/
+	file_size = PAGE_SIZE * 10000;	
+	printf("file size: %d.\n", file_size);
+
+	info.iter = BASE_ITER / 4;
+	info.name = "huge write";
+	one_line_test(fp, copy, write_test, &info);
+
+	info.iter = BASE_ITER;
+	info.name = "huge read";
+	one_line_test(fp, copy, read_test, &info);
+	
+	info.iter = BASE_ITER * 10;
+	info.name = "huge mmap";
+	one_line_test(fp, copy, mmap_test, &info);
+	
+	info.iter = BASE_ITER / 4; 
+	info.name = "huge munmap";
+	one_line_test(fp, copy, munmap_test, &info);
+
+	info.iter = BASE_ITER * 5;
+	info.name = "huge page fault";
+	one_line_test(fp, copy, page_fault_test, &info);
+
+	/*****************************************/
+	/*              WRITE & READ             */
+	/*****************************************/
+
+	/****** SMALL ******/
+	fd_count = 10;
+
+	info.iter = BASE_ITER * 10;
+	info.name = "select";
+	one_line_test(fp, copy, select_test, &info);
+	
+	info.iter = BASE_ITER * 10;
+	info.name = "poll";
+	one_line_test(fp, copy, poll_test, &info);
+		
+	info.iter = BASE_ITER * 10;
+	info.name = "epoll";
+	one_line_test(fp, copy, epoll_test, &info);
+	
+
+	/****** BIG ******/
+	fd_count = 1000;
+
+	info.iter = BASE_ITER;
+	info.name = "select big";
+	one_line_test(fp, copy, select_test, &info);
+
+	info.iter = BASE_ITER;
+	info.name = "poll big";
+	one_line_test(fp, copy, poll_test, &info);
+
+	info.iter = BASE_ITER;
+	info.name = "epoll big";
+	one_line_test(fp, copy, epoll_test, &info);
 
 	fclose(fp);
 	if (!isFirstIteration)
