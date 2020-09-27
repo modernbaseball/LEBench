da1e5dafe8e928920eba548907e0ef544e85735d
rami <rami@node0.darksouls.advosuwmadison-pg0.wisc.cloudlab.us>
2020-09-27 16:28:59 -0500

    omg

--- gho.sh
+++ gho.sh
@@ -1,5 +1,5 @@
 # this fix didnt even work but i will do it anyway just in case
-if [ ! -e /TEST_DIR ]; then mkdir /TEST_DIR; fi
+if [ ! -e /TEST_DIR ]; then sudo mkdir /TEST_DIR; fi
 
 export LEBENCH_DIR=~/LEBench/
 export PATH=/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
